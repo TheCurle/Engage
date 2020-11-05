b36a165749444f51f405bdbe819432467ac76e92
Curle <curle@gemwire.uk>
2020-11-05 22:48:17 +0000

    Datagen and Dilithium model

--- src/main/java/uk/gemwire/engage/datagen/ItemsGen.java
+++ src/main/java/uk/gemwire/engage/datagen/ItemsGen.java
@@ -1,8 +1,9 @@
 package uk.gemwire.engage.datagen;
 
 import net.minecraft.data.DataGenerator;
+import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.*;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import uk.gemwire.engage.Engage;
 import uk.gemwire.engage.registry.RegistryObjects;
@@ -11,6 +12,9 @@ import javax.annotation.Resource;
 
 public class ItemsGen extends ItemModelProvider {
 
+
+    private ItemModelBuilder transformParent;
+
     public ItemsGen(DataGenerator generator, ExistingFileHelper helper) {
         super(generator, Engage.MODID, helper);
     }
@@ -18,7 +22,26 @@ public class ItemsGen extends ItemModelProvider {
     @Override
     protected void registerModels() {
         Engage.LOGGER.info("Generating items!");
-        singleTexture(RegistryObjects.DILITHIUM_ITEM.get().getRegistryName().getPath(), new ResourceLocation("item/handheld"),
-                "layer0", new ResourceLocation(Engage.MODID, "item/dilithium_item"));
+
+        transformParent = withExistingParent("materials_transform", new ResourceLocation("item/generated"));
+        transformParent.transforms()
+                .transform(ModelBuilder.Perspective.FIRSTPERSON_RIGHT)
+                    .translation(0F, 1.25F, -2.5F)
+                    .scale(1, 1, 1)
+                    .end()
+                .end();
+
+        transformedItem(RegistryObjects.DILITHIUM_ITEM.get());
     }
+
+    void transformedItem(Item i) {
+        itemHelper(i, transformParent);
+    }
+
+    void itemHelper(Item i, ModelFile parent) {
+        final ResourceLocation location = new ResourceLocation(i.getRegistryName().getNamespace(), ModelProvider.ITEM_FOLDER + "/" + i.getRegistryName().getPath());
+        ItemModelBuilder builder = factory.apply(location).parent(parent);
+        generatedModels.put(location, builder.texture("layer0", location));
+    }
+
 }
